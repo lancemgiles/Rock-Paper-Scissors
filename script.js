2207229c76088733492446cac30141bf8a3bf5d3
Lance <dogmandevices@gmail.com>
2024-03-27 15:33:48 -0500

    add player selection and play one round

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 function getComputerChoice() {
-	const choice = ['Rock', 'Paper', 'Scissors'];
+	const choice = ['rock', 'paper', 'scissors'];
 	const random = Math.floor(Math.random() * choice.length);
 	return choice[random];
 }
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+	let player = playerSelection.toLowerCase();
+	if (player == computerSelection) {
+		return "Draw!";
+	} else if (player == "rock" && computerSelection == 'scissors') {
+		return "Player wins! Rock beats scissors.";
+	} else if (player == "paper" && computerSelection == "rock") {
+		return "Player wins! Paper beats rock.";
+	} else if (player == "scissors" && computerSelection == "paper") {
+		return "Player wins! Scissors beats paper.";
+	} else {
+		return "Computer wins";
+	}
+
+}
+console.log(playRound('rock', getComputerChoice()));
