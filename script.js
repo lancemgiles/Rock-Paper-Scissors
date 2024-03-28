d7be607ce1380426b27544dae8936c4b619ca19a
Lance <dogmandevices@gmail.com>
2024-03-27 23:08:35 -0500

    track and update scores

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const scissors = document.querySelector("#scissors");
 const scissorsImg = "./images/scissors.png";
 let outcome = document.querySelector(".result");
 let computerImg = document.querySelector("#computerImg");
+const playerScoreBoard = document.querySelector("#playerScore");
+const computerScoreBoard = document.querySelector("#computerScore");
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
 	const choice = ['rock', 'paper', 'scissors'];
@@ -25,27 +29,39 @@ function playRound(playerSelection, computerSelection) {
 	if (playerSelection == computerSelection) {
 		return "Draw!";
 	} else if (playerSelection == "rock" && computerSelection == 'scissors') {
+		playerScore++;
 		return "You win! Rock beats scissors.";
 	} else if (playerSelection == "paper" && computerSelection == "rock") {
+		playerScore++;
 		return "You win! Paper beats rock.";
 	} else if (playerSelection == "scissors" && computerSelection == "paper") {
+		playerScore++;
 		return "You win! Scissors beats paper.";
 	} else {
+		computerScore++;
 		return "Computer wins!";
 	}
+}
+
+function updateScores() {
+	playerScoreBoard.textContent = playerScore;
+	computerScoreBoard.textContent = computerScore;
 }
 
 rock.addEventListener("click", () => {
 	let result = playRound("rock", getComputerChoice());
 	outcome.textContent = result;
+	updateScores();
 });
 
 paper.addEventListener("click", () => {
 	let result = playRound("paper", getComputerChoice());
 	outcome.textContent = result;
+	updateScores();
 });
 
 scissors.addEventListener("click", () => {
 	let result = playRound("scissors", getComputerChoice());
 	outcome.textContent = result;
+	updateScores();
 });
